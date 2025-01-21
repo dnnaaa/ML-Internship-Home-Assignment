10e23acb2ec7a21a089f9e5ea5a94b954117c783
Anouzlay <anouzlay@gmail.com>
2025-01-21 04:20:46 +0100

    <Feature> <Reafactoring> : This file contains the base Components over all the application plus some helperFunction that gonna be used inside some components

--- Component/base_component.py
+++
@@ -1,7 +0,0 @@
-class DashboardComponent:
-    def render(self):
-        """
-        Render the component in the Streamlit dashboard.
-        This method should be overridden by all derived components.
-        """
-        raise NotImplementedError("Subclasses must implement the render method.")
