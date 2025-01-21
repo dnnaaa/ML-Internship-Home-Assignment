eecbca9a0b150eaae798a1f0fb71bc21676ade4c
Anouzlay <anouzlay@gmail.com>
2025-01-21 05:05:16 +0100

    <Reafactoring> <Feature>  : In this updated version , i modified the model from the NaiveBayesModel into logisticModel , plus i change PIPELINE_PATH .Finally in this updated version i add some new Endpoint to make sure that the application gonna save the result into database , and retrieve the informations from the database.

--- data_ml_assignment/api/inference_route.py
+++ data_ml_assignment/api/inference_route.py
@@ -1,17 +1,30 @@
 from fastapi import APIRouter
-
-from data_ml_assignment.api.schemas import Resume
-from data_ml_assignment.models.naive_bayes_model import NaiveBayesModel
+from data_ml_assignment.api.schemas import Resume , PredictionResponse 
+from data_ml_assignment.models.naive_bayes_model import NaiveBayesModel 
+from data_ml_assignment.models.logistic_model import LogisticModel
 from data_ml_assignment.constants import NAIVE_BAYES_PIPELINE_PATH
+from data_ml_assignment.constants import LOGISTIC_MODEL_PIPELINE_PATH
+from .database import save_prediction_to_db, get_all_predictions
+from fastapi import FastAPI, HTTPException
 
-
-model = NaiveBayesModel()
-model.load(NAIVE_BAYES_PIPELINE_PATH)
-
+model = LogisticModel()
+model.load(LOGISTIC_MODEL_PIPELINE_PATH)
 inference_router = APIRouter()
 
 
+#EndPoint to run the inferce or the predict function
 @inference_router.post("/inference")
 def run_inference(resume: Resume):
     prediction = model.predict([resume.text])
     return prediction.tolist()[0]
+
+#EndPoint to save the inference on the database
+@inference_router.post("/save")
+def save_inference(prediction: PredictionResponse):
+    save_prediction_to_db(prediction.text, prediction.predict)
+
+#EndPoint to show all the inferences from the database
+@inference_router.post("/get")
+def get_inference():
+    predictions = get_all_predictions()
+    return {"predictions": predictions}
