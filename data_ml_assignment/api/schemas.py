17c6e69c156f0215cf9e12c2a32f97376568023c
Theuchiwa <53660048+Anouzlay@users.noreply.github.com>
2025-01-21 20:56:36 +0100

    Update schemas.py

--- data_ml_assignment/api/schemas.py
+++ data_ml_assignment/api/schemas.py
@@ -2,3 +2,8 @@ from pydantic import BaseModel
 
 class Resume(BaseModel):
     text: str
+    
+# Response model for predictions
+class PredictionResponse(BaseModel):
+    text: str
+    predict: str
